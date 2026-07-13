735be31a652d63d1c692dd03d3be31a674c07a26
lixingliangsy <154059826+lixingliangsy@users.noreply.github.com>
2026-07-14 07:23:01 +0800

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Note
+  // Note
+  
+  // Note
+  images: {
+    unoptimized: true,
+  },
+  
+  // Note
+  distDir: '.next',
+}
+
+module.exports = nextConfig
